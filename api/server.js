9084da90a0a893a30b6f081b1e9c76d688652369
CryptoN999 <nicholasgallucci4@gmail.com>
2020-02-20 20:09:58 -0800

    mvp

--- api/server.js
+++ api/server.js
@@ -7,12 +7,12 @@ const usersRouter = require('../users/users-router.js');
 
 const server = express();
 
-// server.use(helmet());
-// server.use(express.json());
-// server.use(cors());
+server.use(helmet());
+server.use(express.json());
+server.use(cors());
 
-// server.use('/api/auth', authRouter);
-// server.use('/api/users', usersRouter);
+server.use('/api/auth', authRouter);
+server.use('/api/users', usersRouter);
 
 server.get('/', (req, res) => {
   res.send("Beep!");
